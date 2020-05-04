b4b7e45d90b78d11697c339f2591be91cdd72ef9
Roman A. Nosov <roman@nosoff.info>
2020-05-04 18:25:03 +0300

    fix highlight 3

---
+++ _next/static/e93UP290SyN5aRe2Q1tlH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/posts/[slug]":[a,"static\u002Fcss\u002Fc77a6c1079e98ec5fcc2.css"]}}("static\u002Fchunks\u002F3d916f31d231ee98292379ff5f977a3dac2ac85c.6a7a9cd7b44bec54433d.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
