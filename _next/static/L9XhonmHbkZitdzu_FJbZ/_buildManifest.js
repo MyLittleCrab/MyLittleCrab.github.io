60b9676015b541baf457146cc1ce2e7a635fa599
Roman A. Nosov <roman@nosoff.info>
2020-05-04 17:53:51 +0300

    post about comments

--- _next/static/L9XhonmHbkZitdzu_FJbZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/posts/[slug]":[a,"static\u002Fcss\u002F80627562801ee64d1d9a.css"]}}("static\u002Fchunks\u002F1a3192f544a64cca15d839bf502bdaecc4d130d7.adf63ed0b4b0880115b0.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/xUOUepUazMJtJ_SRVzC_P/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/posts/[slug]":[a,"static\u002Fcss\u002F80627562801ee64d1d9a.css"]}}("static\u002Fchunks\u002F409a8c7e721052d427ef9cc844b96cf0c6954c2d.adf63ed0b4b0880115b0.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
