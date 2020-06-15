d7ad7ec2b34c19599c6a8de472d1a7354c3739ed
JustAddAcid <roman@nosoff.info>
2020-06-15 18:46:10 +0000

    Automated deployment to GitHub Pages: 0dc55b1123aecd8d169bd195e60b114efed3dd31

---
+++ _next/static/OCfqamqRaGVHHYvwifO45/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/posts/[slug]":[a,"static\u002Fcss\u002Fc77a6c1079e98ec5fcc2.css"]}}("static\u002Fchunks\u002F2e7c7441af8abfd95271937538eff9f531e7b604.a375015dfeaf3415b9d5.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
